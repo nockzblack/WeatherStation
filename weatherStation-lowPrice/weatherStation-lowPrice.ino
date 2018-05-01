89bda2b9e7373308da9f763e2fbd24fd0ba5b46c
Fernando Benavides <nockzblack@gmail.com>
2018-04-30 19:52:04 -0500

    hudmity DTH11 sensor code added

--- weatherStation-lowPrice/weatherStation-lowPrice.ino
+++ weatherStation-lowPrice/weatherStation-lowPrice.ino
@@ -1,21 +1,36 @@
 //  LIBRARIES
-#include <OneWire.h>                
-#include <DallasTemperature.h>
+#include <OneWire.h> // DS18B20 temp sensor            
+#include <DallasTemperature.h> // DS18B20 temp sensor 
 
-OneWire auxWire(3); // pin 3 on Arduino
+#include "DHT.h" // DTH11 ambiental humidty sensor  
+
+OneWire auxWire(3); // temp sentor to pin 3 on Arduino
 DallasTemperature tempSensor(&auxWire);
+
+DHT dht(4, DHT11); // ambiental humidty sentor to pin 4 on Arduino
 
 void setup() {
   
   Serial.begin(9600); // Initializing serial comunication
   
   tempSensor.begin(); // Initializing temperature sensor (DS18B20)
-
+  dht.begin(); // Initializing ambiental humidity sensor (DTH11)
 }
 
 void loop() {
-  float ambientalTemp = getTemp();
+  float currentTemp = getTemp();
+  float currentAmbientalHumidity = getAmbientalHumidity();
 
+}
+
+float getAmbientalHumidity() {
+  float ambientalHumidty = dht.readHumidity();
+  if (isnan(ambientalHumidty)) {
+    Serial.println("Falla al leer el sensor DHT11!");
+    return 0;
+  }
+
+  return ambientalHumidty;
 }
 
 float getTemp() {
