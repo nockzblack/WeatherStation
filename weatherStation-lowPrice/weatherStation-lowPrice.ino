dafed6a41f71cb6858115608be4003f89326a5f8
Fernando Benavides <nockzblack@gmail.com>
2018-04-30 20:07:15 -0500

    air Quility and prints to serial monitor added

--- weatherStation-lowPrice/weatherStation-lowPrice.ino
+++ weatherStation-lowPrice/weatherStation-lowPrice.ino
@@ -9,7 +9,7 @@ DallasTemperature tempSensor(&auxWire);
 
 DHT dht(4, DHT11); // declaring ambiental humidity sentor to pin 4 on Arduino
 const int groundHumidity = A0; // declaring ground humidity sensor to analog input A0
-
+const int airQuality = A1; // declaring air Quiality  sensor to analog input A1 on arduino
 
 
 void setup() {
@@ -19,13 +19,36 @@ void setup() {
   tempSensor.begin(); // Initializing temperature sensor (DS18B20)
   dht.begin(); // Initializing ambiental humidity sensor (DTH11)
   pinMode(groundHumidity, INPUT); // Initializing ground humidity sensor
-  
+   pinMode(airQuality, INPUT); // Initializing air Quality  sensor
 }
 
 void loop() {
   float currentTemp = getTemp();
   float currentAmbientalHumidity = getAmbientalHumidity();
   int currentGroundHumidity = getGroundHumidity();
+  int currentAirQuality = getAirQuality();
+
+  Serial.print("Temp = ");
+  Serial.print(currentTemp);
+  Serial.println("ºC");
+  
+  Serial.print("Ambiental Humidity = ");
+  Serial.print(currentAmbientalHumidity);
+  Serial.println("%");
+  
+  Serial.print("Ground Humidity = ");
+  Serial.print(currentGroundHumidity);
+  Serial.println("Escala desconocina");
+  
+  Serial.print("Air Quality = ");
+  Serial.print(currentAirQuality);
+  Serial.println("*Particulas por Millon");
+
+  delay(500);
+}
+
+int getAirQuality() {
+  return analogRead(airQuality);
 }
 
 int getGroundHumidity() {
