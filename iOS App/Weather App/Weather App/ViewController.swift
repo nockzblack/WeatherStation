357318c200640c4da5ae75164439b20b3193bade
Fernando Benavides <nockzblack@gmail.com>
2018-07-19 18:55:34 -0500

    Add IBOutlets and IBActions to the controller and the func that makes the request to the arduino server

--- iOS App/Weather App/Weather App/ViewController.swift
+++ iOS App/Weather App/Weather App/ViewController.swift
@@ -9,10 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Vars
+    
+    
+    // IBOutlets
+    @IBOutlet weak var tempDataLabel: UILabel!
+    
+    @IBOutlet weak var airHumidityDataLabel: UILabel!
+    
+    @IBOutlet weak var airQualityDataLabel: UILabel!
+    
+    @IBOutlet weak var humidityDataLabel: UILabel!
+    
+    
+    // IBActions
+    @IBOutlet weak var updateDataValues: UIButton!
+    
+    
+    // Funcs
+    
+    func requestDataFromServer(urlToRequestStr:String) {
+        
+        let auxURL = URL(string: urlToRequestStr)
+        
+        let task = URLSession.shared.dataTask(with: auxURL!) { (dataReceived, answer, possibleError) in
+            
+            if possibleError != nil {
+                print(possibleError!)
+            } else {
+                let dataStr = NSString(data: dataReceived!, encoding: String.Encoding.utf8.rawValue)
+                
+                print(dataStr!)
+                
+                
+                
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // System funcs
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        requestDataFromServer(urlToRequestStr: "http://192.168.1.200")
     }
 
     override func didReceiveMemoryWarning() {
