6744d6b85c99ae3e3624b3fc97c2d48e0324ffbb
Fernando Benavides <nockzblack@gmail.com>
2018-07-20 13:09:33 -0500

    Extracting temp data from arduino server... A new func was created this returns a Double with the data in a string after an identifier

---
+++ iOS App/Weather App/Weather App/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Weather App
+//
+//  Created by Fernando Benavides Rodriguez on 7/20/18.
+//  Copyright © 2018 Nockzblack. All rights reserved.
+//
+
+import Foundation
